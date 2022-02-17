63e5962263baa9594f2aa724956548aa18fb9bbb
Abdulaziz <abdulazizcloud@gmail.com>
2022-02-18 00:02:02 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react'
 import Layout from './hoc/Layout/Layout';
+import { Route, Switch } from 'react-router-dom';
+import Quiz from './containers/Quiz/Quiz';
 import './App.css';
+import QuizList from "./containers/QuizList/QuizList";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import Auth from "./containers/Auth/Auth";
 
 class App extends Component {
   render() {
     return (
+
    <Layout>
-
-
-     <div style={{width: 400, border: '1px solid black'}}>
-       <h1>Layout works</h1>
-     </div>
+     <Switch>
+       <Route path='/auth' component={Auth}/>
+       <Route path='/quiz-creator' component={QuizCreator}/>
+       <Route path='/quiz/:id' component={Quiz}/>
+       <Route path='/' component={QuizList}/>
+     </Switch>
    </Layout>
   );
   }
